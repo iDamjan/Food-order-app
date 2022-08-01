d478ace666ae7d87f99567c5cc314aa9d897ce9c
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2022-08-01 18:18:59 +0200

    Implemented functionality for adding and removing items from the cart component

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -8,7 +8,9 @@ const defaultCartState = {
 };
 
 const cartReducer = (state, action) => {
+
   if (action.type === "ADD") {
+
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
@@ -20,6 +22,7 @@ const cartReducer = (state, action) => {
     let updatedItems;
 
     if (existingCartItem) {
+      
       const updatedItem = {
         ...existingCartItem,
         amount: existingCartItem.amount + action.item.amount,
@@ -35,7 +38,25 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
-
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if(existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id)
+    } else {
+      const updatedItem = {...existingItem, amount: existingItem.amount -1}
+      updatedItems = [...state.items]
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return{
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
   return defaultCartState;
 };
 
